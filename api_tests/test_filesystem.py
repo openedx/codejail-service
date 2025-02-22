9dcc8a82043d1c446982e299685d39e58f6a123f
Tim McCormack <tmccormack@edx.org>
2025-02-22 23:39:08 +0000

    fixup! Expand filesystem tests

--- api_tests/test_filesystem.py
+++ api_tests/test_filesystem.py
@@ -5,6 +5,9 @@ Mostly should be denied, but parts of sandbox should be allowed.
 """
 
 from textwrap import dedent
+from unittest import TestCase
+
+import ddt
 
 from api_tests.utils import call_api_code_error, call_api_success
 
@@ -34,15 +37,60 @@ def test_can_write_in_own_sandbox():
     assert {"out": "sample"} == call_api_success(code, {})
 
 
-def test_cannot_read_os_files():
+@ddt.ddt
+class TestFilesystemDenial(TestCase):
     """
-    We can't read most files out in the broader OS.
+    Various wider-OS filesystem operations denied to us.
     """
-    # Some file that's fairly harmless and that wouldn't have been
-    # *specifically* blocked.
-    code = dedent("""
-      with open("/etc/hosts", 'r') as f:
-          f.read()
-    """)
-    (_, emsg) = call_api_code_error(code, {})
-    assert "PermissionError: [Errno 13] Permission denied: \\'/etc/hosts\\'" in emsg
+
+    @ddt.data(
+        # Some file that's fairly harmless and that wouldn't have been
+        # *specifically* blocked.
+        "/etc/hosts",
+        # Another quasi-harmless area of the filesystem
+        "/proc/1/cmdline",
+    )
+    def test_cannot_read_os_files(self, file_path):
+        """
+        We can't read most files out in the broader OS.
+        """
+        code = dedent(f"""
+          with open({file_path!r}, 'r') as f:
+              f.read()
+        """)
+        (_, emsg) = call_api_code_error(code, {})
+        assert f"PermissionError: [Errno 13] Permission denied: \\'{file_path}\\'" in emsg
+
+    @ddt.data(
+        # Generic OS paths
+        "/sys",
+        "/",
+        "/tmp",
+        # Parent directory contains other users' codejails
+        "../",
+    )
+    def test_cannot_list_os_dirs(self, dir_path):
+        """
+        We can't list various directories.
+        """
+        code = dedent(f"""
+          import os
+          out = os.listdir({dir_path!r})
+        """)
+        (_, emsg) = call_api_code_error(code, {})
+        assert f"PermissionError: [Errno 13] Permission denied: \\'{dir_path}\\'" in emsg
+
+    @ddt.data(
+        "/tmp/apitest.txt",
+        "../apitest.txt",
+    )
+    def test_cannot_write_outside_sandbox(self, file_path):
+        """
+        We can't write to various places outside the sandbox.
+        """
+        code = dedent(f"""
+          with open({file_path!r}, 'w') as f:
+              f.write("test")
+        """)
+        (_, emsg) = call_api_code_error(code, {})
+        assert f"PermissionError: [Errno 13] Permission denied: \\'{file_path}\\'" in emsg
